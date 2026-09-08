09109db54d431605f745f088bf68bdf4f0ceb36c
saidur <saidurrahman1004@gmail.com>
2026-09-08 11:40:45 +0600

    fix: suppress hydration warning caused by browser extensions

--- apps/storefront/src/app/[locale]/layout.tsx
+++ apps/storefront/src/app/[locale]/layout.tsx
@@ -46,7 +46,7 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className={`${plusJakartaSans.variable} ${hindSiliguri.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <NextIntlClientProvider messages={messages}>
           <StorefrontShell locale={locale}>{children}</StorefrontShell>
         </NextIntlClientProvider>
